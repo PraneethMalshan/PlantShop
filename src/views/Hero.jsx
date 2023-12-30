69e0025f8f374eef514a9d9f3f8d848e8b3bec86
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-12-30 14:15:17 +0530

    Added logo

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import OpenPG from "../assets/OpenPG.jpg"
+import MHomeLogo from "../assets/MHomeLogo.png";
 
 const Hero = () => {
   return <div className="h-screen relative flex flex-col items-center"
@@ -9,9 +10,13 @@ const Hero = () => {
         backgroundPosition: "bottom",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-  }}>
+  }}
+  >
+    <div className="relative w-full max-w-[1490] flex items-center justify-between pt-10 mx-auto px-10">
+      <img src={MHomeLogo} alt=""/>
 
-
+      <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]"></ul>
+    </div>
   </div>;
 };
 
