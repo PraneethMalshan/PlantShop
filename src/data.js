72dd18ce065f6e7a2234cbc2cc88d5e16049b021
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-01-07 08:29:10 +0530

    Services page is done

--- src/data.js
+++ src/data.js
@@ -1,7 +1,30 @@
+import shippingIcon from "./assets/free-shiping-icon.svg";
+import paymentIcon from "./assets/quick-payment-icon.svg";
+import supportIcon from "./assets/support-icon.svg";
+
 
 export const heroTitle = "Bring Serenity to Your Place With Interior Plants";
 export const heroSubtitle =
   "find your dream plant for your home decoration with us, and we will make it happen.";
+
+
+  export const services = [
+    {
+      title: "Free Shipping",
+      subtitle: "No charge for your delivery",
+      icon: shippingIcon,
+    },
+    {
+      title: "Quick Payment",
+      subtitle: "100% secure",
+      icon: paymentIcon,
+    },
+    {
+      title: "24/7 Support",
+      subtitle: "Don't hesitate to contact us",
+      icon: supportIcon,
+    },
+  ];
 
 
 
